5c3839e7c59be75774f386b891bf6a2cfb53b622
Symion Edwards <symion.edwards@gmail.com>
2020-10-30 14:29:29 +0000

    Completed the 6th user story to define a Dice method to return the total.

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,7 +1,11 @@
 class Dice
     def roll(argument = 1)
         results = []
-        argument.times {results << rand(6) + 1}
+        argument.times{results << rand(6) + 1}
         return results
+    end
+
+    def total(roll)
+        roll.inject(:+)
     end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -14,21 +14,30 @@ describe Dice do
     # Not necessary for this workshop
     # Instead it was sufficient to do a feature test in IRB. 
 
-    it 'the roll method returns a number between 1 and 6' do
-        expect(subject.roll).to be_between(1,6)
-    end
+    #it 'the roll method returns a number between 1 and 6' do
+    #    expect(subject.roll).to be_between(1,6)
+    #end
+    #This test has been superceded by another test
 
     it 'user can specify how many times they would like to roll the dice' do
     # Tried this: allow_any_instance_of(Dice).to receive(:roll) 
     # This didn't work as it was basically the same as allows user to roll dice test
         expect(subject).to respond_to(:roll).with(1).argument
     end
-
+    
     it 'shows the results of each dice roll' do
-        expect(subject.roll(7)).to eq 7
+        expect(subject.roll(50)).to all(be_between(1,6))        
     end
 
-    #expect(subject.roll(15) to eq 15)
+    # User Story 6 - I want to be able to get my current score
+    # This is defined on the Dice class so to find out what the score would be --> dice.total(roll)
+    it 'responds to the method total' do
+        expect(subject).to respond_to(:total)
+    end
+    it "gives a total of all the rolled dice" do
+        expect(subject.total([1, 2, 3])).to eq(6)
+    end
 
-    #expect(subject.roll_history.length).to eq 15
 end
+
+    
